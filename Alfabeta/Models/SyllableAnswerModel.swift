fb4782c5ecbfc3333febbef1c69ba68b319042f6
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-06-22 16:54:47 -0300

    updates

--- Alfabeta/Models/SyllableAnswerModel.swift
+++ Alfabeta/Models/SyllableAnswerModel.swift
@@ -8,4 +8,38 @@
 import SwiftUI
 
 
-
+class SyllableAnswerModel: Hashable{
+    
+    let id: UUID
+    let syllable: String
+    let sound: SoundOption
+    let buttonColor: Color
+    let textColor: Color
+    let isCorrect: Bool
+    
+    
+    init(syllable: String, sound:SoundOption, buttonColor:Color, textColor:Color, isCorrect: Bool){
+        
+        self.id = UUID()
+        self.syllable = syllable
+        self.sound = sound
+        self.buttonColor = buttonColor
+        self.textColor = textColor
+        self.isCorrect = isCorrect
+    }
+    
+    static func == (lhs: SyllableAnswerModel, rhs: SyllableAnswerModel) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(syllable)
+        hasher.combine(sound)
+        hasher.combine(buttonColor)
+        hasher.combine(textColor)
+        hasher.combine(isCorrect)
+        
+    }
+    
+}
