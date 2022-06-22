1ac7af5e6049af9482986b8241c8353026a66050
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-06-22 17:06:42 -0300

    updates

--- Alfabeta/Models/SyllableSentenceModel.swift
+++ Alfabeta/Models/SyllableSentenceModel.swift
@@ -8,4 +8,28 @@
 import SwiftUI
 
 
-
+class SyllableSentenceModel: Hashable{
+    
+    let id: UUID
+    let syllable: String
+    let toGuess: Bool
+    
+    
+    init(syllable: String, toGuess: Bool = false) {
+        self.id = UUID()
+        self.syllable = syllable
+        self.toGuess = toGuess
+    }
+    
+    static func == (lhs: SyllableSentenceModel, rhs: SyllableSentenceModel) -> Bool {
+        return lhs.id == rhs.id
+        
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(syllable)
+        hasher.combine(toGuess)
+        
+    }
+}
